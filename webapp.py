da2bb9d3d90c9ecc0beee2f3905a12d20811c1b3
Loai17 <loai.qubti@gmail.com>
2018-10-01 16:38:33 +0300

    updating

--- webapp.py
+++ webapp.py
@@ -9,6 +9,11 @@ import datetime
 import os
 from forms import ContactForm
 import pyperclip
+#Firebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
 
 # Flask Mail
 from flask_mail import Message, Mail
@@ -40,6 +45,31 @@ engine = create_engine('sqlite:///database.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine, autoflush=False)
 session = DBSession()
+
+# FIREBASE
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': 'boom-boom-pass-website',
+})
+
+db = firestore.client()
+
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'Ada',
+    u'last': u'Lovelace',
+    u'born': 1815
+})
+  
+doc_ref = db.collection(u'users').document(u'aturing')
+doc_ref.set({
+    u'first': u'Alan',
+    u'middle': u'Mathison',
+    u'last': u'Turing',
+    u'born': 1912
+})
+snippets.py
+
 
 def allowed_file(filename):
 	return '.' in filename and \
